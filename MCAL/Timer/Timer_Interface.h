9ffb7ab49524c790ae37299ad17c372b79a07fb8
Youssef Hussein <youssefismail1>
2022-08-25 21:34:33 +0200

    1.0.0

--- MCAL/Timer/Timer_Interface.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef TIMER_INTERFACE_H_
-#define TIMER_INTERFACE_H_
-
-
-void Timer1_OneShotMode(u8 times);
-void Timer0_PerodicMode(void);
-
-#endif
--- common/MACROS.h
+++
@@ -1,9 +0,0 @@
-#ifndef  MACROS_H_
-#define  MACROS_H_
-
-#define Set_Bit(reg,bit)    reg|=(1<<bit)
-#define Clear_Bit(reg,bit)  reg&=(~(1<<bit))
-#define Get_Bit(reg,bit)    ((reg>>bit)&1)
-#define Toggle_Bit(reg,bit)  reg^=(1<<bit)
-
-#endif
